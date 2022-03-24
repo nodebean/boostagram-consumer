ae8dba6fa4f20c2164f8ded57e59ef9f5cd5c494
nodebean <nodebean@beanteam.org>
2022-03-24 22:54:03 +0000

    added CAD

--- app/app.py
+++ app/app.py
@@ -16,11 +16,16 @@ MIN_SATS = int(os.environ['MIN_SATS'])
 BOT_USER_NAME = os.environ['BOT_USER_NAME']
 
 def get_sat_value(sats):
-    cb_response = requests.get('https://api.coinbase.com/v2/prices/spot?currency=USD').json()
-    usd_price = float(cb_response['data']['amount'])
-    sat_value = usd_price / 100000000
-    sat_value = sats * sat_value 
-    return sat_value
+    usd_response = requests.get('https://api.coinbase.com/v2/prices/spot?currency=USD').json()
+    usd_price = float(usd_response['data']['amount'])
+    usd_sat_value = usd_price / 100000000
+    usd_value = sats * usd_sat_value
+
+    cad_response = requests.get('https://api.coinbase.com/v2/prices/spot?currency=CAD').json()
+    cad_price = float(cad_response['data']['amount'])
+    cad_sat_value = cad_price / 100000000
+    cad_value = sats * cad_sat_value
+    return [usd_value, cad_value]
 
 def data_validator(raw_message):
     if raw_message['value_msat'] < MIN_SATS:
@@ -35,7 +40,7 @@ def format_message(raw_message):
         sats = int(sats) / 1000
         sats = round(sats)
         sats_value = get_sat_value(sats)
-        formatted_message = f"{sender} sent a donation of {sats} sats (~${sats_value:.2f}) with the message: {text}"
+        formatted_message = f"{sender} sent a donation of {sats} sats (~${sats_value[0]:.2f} USD / ~${sats_value[1]:.2f} CAD) with the message: {text}"
         return formatted_message
     except:
         return "Somebody donated some sats, but the details were lost in transit. Sorry."
